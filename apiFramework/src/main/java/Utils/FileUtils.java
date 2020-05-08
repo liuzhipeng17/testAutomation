2f490cf665ad4ec5bcfd0c498de0c4072159d58b
liuzhipeng <liucpliu@sina.cn>
2020-05-08 11:25:41 +0800

    no message

--- apiFramework/src/main/java/Utils/FileUtils.java
+++
@@ -1,8 +0,0 @@
-package Utils;
-
-public class FileUtils {
-
-    public static String getFilePath(String fileName){
-        return Thread.currentThread().getContextClassLoader().getResource(fileName).getPath();
-    }
-}
